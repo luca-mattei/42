908c08263188a08508fd3c9f3d7fdf54c8ff1ac7
luca-mattei <74422412+luca-mattei@users.noreply.github.com>
2024-11-26 14:55:30 +0100

    Add files via upload

---
+++ checkprintf/libft/ft_lstdelone_bonus.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lumattei <lumattei@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 03:55:45 by lumattei          #+#    #+#             */
+/*   Updated: 2024/10/13 03:59:38 by lumattei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (lst && del)
+	{
+		del(lst->content);
+		free(lst);
+	}
+}
